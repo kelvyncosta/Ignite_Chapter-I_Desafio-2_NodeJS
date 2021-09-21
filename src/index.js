924db45f6caa2bbec5202fd85f60371cf2c58aea
Kelvyn Costa <kelvyn.costa@essencebr.com>
2021-09-21 11:18:50 -0300

    All middlewares implementation

--- src/index.js
+++ src/index.js
@@ -10,19 +10,80 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  if (!username) {
+    return response.status(403).json({ error: 'User must be specified' });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: 'User must be pro to create a more than 10 todos'
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  if (!username) {
+    return response.status(403).json({ error: 'User must be specified' });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+  
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'The ID must be an UUID' });
+  }
+
+  const todo = user.todos.find(obj => obj.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'This todo does not belong to this user'
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(obj => obj.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
